d9482357fbb5d0cc3018074b22d2c62528f405f9
jayasuriya321 <jayasuriya0321@gmail.com>
2025-10-13 15:14:16 +0530

    commit 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://finance-53gy.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   timeout: 10000, // 10 seconds timeout
 });
 
