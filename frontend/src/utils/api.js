ba3c8220da00165c31ac4d52b24cee3ed6056d4e
jayasuriya321 <jayasuriya0321@gmail.com>
2025-10-29 18:49:06 +0530

    frontend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://finance-53gy.onrender.com/api",
   timeout: 10000,
 });
 
